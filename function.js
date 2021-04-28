a73904576d5aeff013c571955258980f62e3b69b
teddypeters <60046320+teddypeters@users.noreply.github.com>
2021-04-27 23:01:30 -0500

    Updates

--- function.js
+++ function.js
@@ -5,6 +5,12 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
 
+function startingFunc() {
+    var x = document.getElementById("myLinks");
+    x.style.display = "none";
+
+}
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
