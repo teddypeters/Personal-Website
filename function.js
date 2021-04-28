1266d739745e84e611197e5a3c130d2576592d64
teddypeters <60046320+teddypeters@users.noreply.github.com>
2021-04-28 15:25:23 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,19 @@
+/* 
+* Author: Theodore Peters
+* Date: 4/17/21
+* JavaScript for teddypeters.github.io
+-----------------------------------------------------------------------------
+*/
+
+
+/* -------------------------------------------(Start Snippet) -----------------------------------------------------------
+JS code for progress bar sourced from https://css-tricks.com/books/greatest-css-tricks/scroll-animation/  (NOT AUTHORED BY MYSELF) */
+
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+/*----------------------------------------------(End Snippet)--------------------------------------------------------*/
+
 
 function startingFunc() {
     var x = document.getElementById("myLinks");
