d8cf2093d37f05fa77e40a2344ddbe91303a73df
teddypeters <60046320+teddypeters@users.noreply.github.com>
2021-04-27 22:54:44 -0500

    Added movement

--- function.js
+++ function.js
@@ -1,2 +1,15 @@
 src = "https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js"
 src = "https://unpkg.com/smoothscroll-anchor-polyfill"
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
